e0b4cb171f2fddb30103ca77c86c4dfbe4d81ce8
kyr0 <info@aron-homberg.de>
2019-10-29 11:18:44 +0100

    feat(system:command,command@version): And better docs

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 > 💨Just like `npx` but chainable, way faster and always using `@latest` of any command. 💨
 
-> 🎉 Get rid of all npm script to chain commands. 🎉
+> 🎉 Get rid of all node scripts to chain commands 🎉
 
 > 🚀Get rid of direct & out-dated CLI dependencies. 🚀
 
 > 🧑‍🤝‍🧑 Cross-platform and end-2-end tested. ✨
+
+#### Performance
 
 ~2x faster than `npx` when your machine is online:
 
@@ -19,19 +21,22 @@
 
 And it always checks for the `@latest` version when your machine is online.
 
+#### Usage
+
 You can use `st-run` in npm scripts like this:
 
 ```
 {
     "name": "your-node-module",
     "scripts": {
-        "copy-smth": "st-run st-cp package.json package.json2 + st-cp package2.json package3.json"
+        "copy-smth": "st-run st-cp package.json package.json2"
     },
     "dependencies": {
         "st-run": "latest"
     }
 }
 ```
+
 
 This way you may *get rid of some direct dependencies* and make sure the CLI tools being _used by end-users are always up to date_.
 
@@ -42,3 +47,28 @@ Or install it globally like `npx`:
 And use it globally just like `npx`:
 
     st-run st-cp package.json package.json2
+
+
+#### `commandA arg1 + commandB arg1 arg2` Chaining Syntax
+
+You can get rid of special node scripts to chain commands by letting `st-run` chain them for you:
+
+    st-run st-cp package.json package.json2 + st-cp package.json2 package.json3
+
+As you can see, a simple `+` chains two or more commands. There is no command count limit.
+
+#### `command@specificVersion` Syntax
+
+With `st-run` you can have multiple cached versions at the same time (just like with `maven`, `gradle` etc.):
+
+    st-run st-cp@1.0.0 package.json package.json2 + st-cp@2.0.0 package.json2 package.json3
+
+Cool?
+
+### `system:command` Syntax
+
+Oh, you want to run a command that is not an npm script? `st-run` got you covered:
+
+    st-run system:rm -rf dist + system:tsc
+
+This also works for globally installed npm packages that you don't want to update on-the-fly.
--- src/install-and-execute.ts
+++ src/install-and-execute.ts
@@ -5,37 +5,76 @@ import { getLatestInstalledVersion } from "./get-latest-installed-version";
 import { getPackageVersionedPath } from "./get-package-versioned-path";
 import { installPackage } from "./install-package";
 import { isOnline } from "./is-online";
+import { isPackageInstalled } from "./is-package-installed";
 export const installAndExecute = async (packageName: string, args: Array<string>) => {
+  // support for system:$command syntax
+  if (packageName.startsWith("system:")) {
+    packageName = packageName.substring(7);
+
+    execute(packageName, args, {
+      stdio: "inherit",
+    });
+    return;
+  }
+
+  // support for command@$specificVersion syntax
+  const packageNameAndVersion = packageName.split("@");
+  let specificRequestedVersion, isSpecificRequestedVersionInstalled;
+
+  if (packageNameAndVersion[1]) {
+    packageName = packageNameAndVersion[0];
+    specificRequestedVersion = packageNameAndVersion[1];
+    isSpecificRequestedVersionInstalled = isPackageInstalled(packageName, specificRequestedVersion);
+  }
+
   const latestInstalledVersion = getLatestInstalledVersion(packageName);
-  let latestAvailableVersion: string;
+  let versionToUse: string;
 
   if (await isOnline()) {
-    latestAvailableVersion = fetchLastestPackageVersion(packageName);
+    if (!specificRequestedVersion) {
+      versionToUse = fetchLastestPackageVersion(packageName);
 
-    if (!latestInstalledVersion) {
-      console.log("[i] Package is not installed. Installing: ", latestAvailableVersion, "...");
-      installPackage(packageName);
-      latestAvailableVersion = fetchLastestPackageVersion(packageName);
+      if (!latestInstalledVersion) {
+        console.log("[i] Package is not installed. Installing: ", versionToUse, "...");
+        installPackage(packageName);
+        versionToUse = fetchLastestPackageVersion(packageName);
+      } else {
+        console.log("[i] Latest installed version is: ", latestInstalledVersion);
+      }
+
+      if (versionToUse !== latestInstalledVersion) {
+        console.log("versionToUse", versionToUse, "!= latestInstalledVersion", latestInstalledVersion);
+
+        console.log("[i] Cached version is out of date. Installing: ", versionToUse, "...");
+        installPackage(packageName, versionToUse);
+        versionToUse = fetchLastestPackageVersion(packageName);
+      }
     } else {
-      console.log("[i] Latest installed version is: ", latestInstalledVersion);
-    }
-
-    if (latestAvailableVersion !== latestInstalledVersion) {
-      console.log("[i] Cached version is out of date. Installing: ", latestAvailableVersion, "...");
-      installPackage(packageName, latestAvailableVersion);
-      latestAvailableVersion = fetchLastestPackageVersion(packageName);
+      if (!isSpecificRequestedVersionInstalled) {
+        console.log("[i] Specific version is not installed. Installing: ", specificRequestedVersion, "...");
+        installPackage(packageName, specificRequestedVersion);
+        versionToUse = specificRequestedVersion;
+      }
     }
   } else {
-    console.log("[!!] Warning: System is offline. Using local cache version: ", latestInstalledVersion);
+    if (!specificRequestedVersion) {
+      console.log("[!!] Warning: Machine is offline. Using local cache version: ", latestInstalledVersion);
 
-    if (!latestInstalledVersion) {
-      console.log("[i] Package is not installed. Exiting.");
-      process.exit(1);
+      if (!latestInstalledVersion) {
+        console.log("[i] Package is not installed. Exiting.");
+        process.exit(1);
+      }
+      versionToUse = latestInstalledVersion;
+    } else {
+      if (!isSpecificRequestedVersionInstalled) {
+        console.log("[i] Specific version is not installed. Exiting.");
+        process.exit(1);
+      }
+      versionToUse = specificRequestedVersion;
     }
-    latestAvailableVersion = latestInstalledVersion;
   }
-  
-  const installedPackagePath = getPackageVersionedPath(packageName, latestAvailableVersion);
+
+  const installedPackagePath = getPackageVersionedPath(packageName, versionToUse);
   const executable = getExecutablePath(packageName, installedPackagePath);
 
   // process.execPath -> node executable
