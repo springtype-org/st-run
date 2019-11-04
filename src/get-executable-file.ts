a0e4c4a347d1614592427d7ed1df6b77bbbd8fce
mansi1 <m.mansi1@gmx.net>
2019-11-04 20:24:46 +0100

    fix(windows): fix deployment path

--- src/get-executable-file.ts
+++ src/get-executable-file.ts
@@ -1,9 +1,13 @@
 import {resolve} from "path";
 import {platform} from "os"
+import {readPackageJson} from "./read-package-json";
 
 export const getExecutablePath = (packageName: string, versionedPackageCachePath: string) => {
     if (platform() === "win32") {
-        return resolve(versionedPackageCachePath, 'node_modules', packageName, 'cli.js');
+        const modulePath = resolve(versionedPackageCachePath, 'node_modules', packageName);
+        const bin = readPackageJson(modulePath).bin || {};
+        const executionJs = bin[packageName];
+        return  resolve(modulePath,executionJs);
     } else {
         return resolve(versionedPackageCachePath, 'node_modules', '.bin', packageName);
     }
---
+++ src/read-package-json.ts
@@ -0,0 +1,6 @@
+import {readFileSync} from "fs";
+import {resolve} from "path";
+
+export const readPackageJson = (modulePath: string): { name: string, bin: { [key: string]: string } } => {
+    return JSON.parse(readFileSync(resolve(modulePath, 'package.json')).toString());
+};
